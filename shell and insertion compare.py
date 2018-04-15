901e44c1dc1de21b36f14a0a1b528cbf1e62350b
T.M <38026296+tattooday@users.noreply.github.com>
2018-04-16 02:29:22 +0800

    Update shell and insertion compare.py

--- shell and insertion compare.py
+++ shell and insertion compare.py
@@ -3,7 +3,8 @@
 
 # In[68]:
 
-
+#details of insertion sort can be found here
+# https://github.com/tattooday/search-and-sort/blob/master/bubble%2C%20selection%20and%20insertion%20sort.py
 def ins(list):
    
     for i in range(1,len(list)):
@@ -20,18 +21,38 @@ def ins(list):
         
     return list
 
+#shell sort is a variation of insertion sort
+#basically i have to use slicing methond [a::b]
+#to cut the list into a few small groups
+#assume we have a list of n elements
+#the first step, we do b=n//a, we get a few list[a::b]
+#we apply insertion sort on those sublists and concatenate
+#next, we do b=b//a, and we get a few new list[a::b]
+#we apply insertion sort on new sublists and concatenate
+#we keep using //a method and do insertion sort and concatenate
+#til b reaches zero
+#we concatenate sublists and do a final insertion sort
+#we obtain a sorted list
+#the time complexity is quite difficult to calculate
 
 def shel(list):
-    
-    b=len(list)//4
+   #the first step is to initialize a
+   #we will use it to divide the list and do slicing
+   #in this case, i use 4
+   #we keep b divided by 4 until it reaches zero
+    a=4
+    b=len(list)//a
     while b>0:
-        final=[0]*len(list)
+         #instead of rewriting insertion sort
+         #i directly use it in shell sort     
         for i in range(b):
             temp=ins(list[i::b])
+            #the final loop is actually the slicing of [a::b]
+            #[a::b] equals to [a+j*b] j for j in range()
             for j in range(len(temp)):
-                final[i+j*b]=temp[j]
-        b=b//4
-    return ins(final)
+                list[i+j*b]=temp[j]
+        b=b//a
+    return ins(list)
 
 
 # In[118]:
@@ -40,6 +61,10 @@ def shel(list):
 import datetime as dt
 import random as rd
 
+#we run two functions to see which one is faster
+#we set n to generate n random variables
+#we concatenate them and pass the list to two different sorts
+#we store the information of each function's processing time
 def compare(n):
     z=[]
     for i in range(n):
@@ -63,7 +88,9 @@ def compare(n):
 # In[133]:
 
 
-
+#i run 20 times for one function
+#then take the population mean
+#eventually print how long each function takes to sort
 def avg(n):
     l1=0
     l2=0
@@ -76,7 +103,9 @@ def avg(n):
 
 
 # In[140]:
-
+#theoretically shell sort should be faster
+#but my code for shell sort is definitely o(n^3)
+#undoubtedly shell sort is slower
 
 avg(10)
 avg(100)
