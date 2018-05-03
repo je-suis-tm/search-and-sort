51fbff809b84a23a3814c5d2af365099732dbb42
T.M <38026296+tattooday@users.noreply.github.com>
2018-05-03 23:48:42 +0800

    Update count and insertion compare.py

--- count and insertion compare.py
+++ count and insertion compare.py
@@ -8,20 +8,39 @@ Created on Thu May  3 15:42:57 2018
 # coding: utf-8
 
 # In[37]:
-
+#counting sort is kinda different
+#it works the best when there is large quantity of duplicates
+#when the maximum value is larger than the length of list
+#it is not economical to use counting sort
+#counting sort is counting how many times each element has repeatedly showed up
+#we create a list of the size of maximum value
+#each index of the list represents the value of elements
+#each value of the list represents the frequency of elements
+#in the end we concatnate each element and its frequency
+#simplest sort among all
 
 def counts(n):
-    
+    #first we get the maximum value
+    #if maximum value is larger than the length of the list
+    #we would create a list with a huge space complexity
+    #its not worth it
+    #we can use a better sort
     maxim=max(n)
     if maxim>len(n):
         print('choose other sorting techniques')
         return
     
+    #we create a list to store the frequency of each element
+    #note that we use maximum value +1
+    #as zero is also taken into consideration
     l=[0]*(maxim+1)
     
+    #this is the calculation of the frequency
     for i in n:
         l[i]+=1
     
+    #lets clear the original list
+    #we add each element and its frequency back to the list
     n=[]
     for j in range(maxim+1):
         if l[j]!=0:
@@ -31,8 +50,7 @@ def counts(n):
 
 
 # In[38]:
-
-
+#the rest is just comparison with our benchmark insertion sort
 def ins(list):
     for i in range(1,len(list)):
         val=list[i]
@@ -45,8 +63,6 @@ def ins(list):
 
 
 # In[39]:
-
-
 import datetime as dt
 import random as rd
 
@@ -75,3 +91,8 @@ def compare(n):
     return
 
 compare(5000)
+
+#the time complexity for counting sort is o(n+k)
+#k is determined by the maximum value
+#its easy to see that counting sort easily beats o(n^2)
+#however its time complexity is much larger
