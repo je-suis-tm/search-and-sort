a614952a40d6b721fdfadfb8a8b13d02bd1afceb
T.M <38026296+tattooday@users.noreply.github.com>
2018-07-01 16:42:38 +0100

    Update sequential and binary search.py

--- sequential and binary search.py
+++ sequential and binary search.py
@@ -9,11 +9,13 @@
 #basically it is just doing a traversal on all items
 def f(n,list):
     i=0
+    
     #we create a boolen value to control the loop
     #when we find the value in list
     #we reset found so we break the loop
     #in the end the output is found
     found=False
+    
     #note that while function does one extra iteration
     #so it is i<len(list) instead of i<=len(list)
     while i<len(list) and not found:
@@ -39,6 +41,7 @@ f(1,[4,5,1])
 #sequential search could be more efficient
 def bina(n,list):
     f=0
+    
     #for binary search
     #we create the first and the last index
     #binary search starts from the middle
@@ -49,12 +52,14 @@ def bina(n,list):
     list=sorted(list)
     found=False
     while f<=l and not found:
+        
         #to get the middle point of any length
         #we need to get the half of the sum of first and last index
         #so we use (f+l)//2 to get thhe middle value for any length
         i=(f+l)//2
         if list[i]==n:
             found=True
+            
             #if the middle value is larger than the target
             #we search the lower half
             #we set l=i-1
@@ -63,6 +68,7 @@ def bina(n,list):
         elif list[i]>n:
             l=i-1
         else:
+            
             #vice versa
             f=i+1
     return found
