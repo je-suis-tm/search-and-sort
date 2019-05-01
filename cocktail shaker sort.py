b041f5eebc0bc99409681782e09fe628db6db797
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-05-01 19:18:57 +0100

    complete comments

--- cocktail shaker sort.py
+++ cocktail shaker sort.py
@@ -10,37 +10,44 @@ import random as rd
 # In[2]:
 
 #cocktail shaker sort is a variation of bubble sort
+#go the link below to check out the details of bubble sort
+# https://github.com/je-suis-tm/search-and-sort/blob/master/bubble,%20selection%20and%20insertion%20sort.py
 #bubble sort repeatedly takes comparison from one side to the other
 #usually left to right, takes n*(n-1) steps, assuming n is the size
 #cocktail shaker sort has a similar mechanism
 #except it compares its elements from left to right
-#then right to left, then left to right
+#then right to left, later left to right
 #the process looks like a cocktail shaker (not really)
 #that is how the sort gets its name
 #in theory, for a partially sorted list
 #cocktail shaker sort takes less time than a bubble sort
 def cocktail_shaker_sort(target):
-
+    
+    #we use left and right to define each round's start and end point
     left=0
     right=len(target)-1
     swap=True
-
-
+    
     while left<right or not swap:
     
         swap=False
-
+        
+        #as usual, we start from left to right
         for i in range(left+1,right+1):
             if target[i]<target[i-1]:
                 target[i],target[i-1]=target[i-1],target[i]
                 swap=True
         right-=1
         
+        #swap is the key to increase the efficiency
+        #once there is no swap happened in the loop
+        #we can conclude the target list is fully sorted
         if not swap:
             return target
         
         swap=False
         
+        #then right to left
         for j in range(right,left,-1):
             if target[j]<target[j-1]:
                 target[j],target[j-1]=target[j-1],target[j]
