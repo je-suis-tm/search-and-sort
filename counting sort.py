cbb0472fde0165ed1a9130393f00fededec9d030
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-05-01 19:58:28 +0100

    restructure

--- counting sort.py
+++ counting sort.py
@@ -7,8 +7,10 @@ Created on Thu May  3 15:42:57 2018
 
 # coding: utf-8
 
-# In[37]:
-#counting sort is kinda different
+# In[1]:
+
+
+#counting sort is kinda different from other sorts in this repo
 #it works the best when there is large quantity of duplicates
 #when the maximum value is larger than the length of list
 #it is not economical to use counting sort
@@ -16,18 +18,18 @@ Created on Thu May  3 15:42:57 2018
 #we create a list of the size of maximum value
 #each index of the list represents the value of elements
 #each value of the list represents the frequency of elements
-#in the end we concatnate each element and its frequency
+#in the end we concatenate each element and its frequency
 #simplest sort among all
 
-def counts(n):
+def counting_sort(target):
     
     #first we get the maximum value
     #if maximum value is larger than the length of the list
     #we would create a list with a huge space complexity
     #its not worth it
     #we can use a better sort
-    maxim=max(n)
-    if maxim>len(n):
+    maxim=max(target)
+    if maxim>len(target):
         print('choose other sorting techniques')
         return
     
@@ -37,63 +39,29 @@ def counts(n):
     l=[0]*(maxim+1)
     
     #this is the calculation of the frequency
-    for i in n:
+    for i in target:
         l[i]+=1
     
     #lets clear the original list
     #we add each element and its frequency back to the list
-    n=[]
+    output=[]
     for j in range(maxim+1):
         if l[j]!=0:
-            n+=[j]*l[j]
+            output+=[j]*l[j]
     
-    return n
+    return output
 
 
-# In[38]:
-#the rest is just comparison with our benchmark insertion sort
-def ins(list):
-    for i in range(1,len(list)):
-        val=list[i]
-        j=i
-        while val<list[j-1] and j!=0:
-                list[j]=list[j-1]
-                j-=1
-        list[j]=val
-    return list
-
-
-# In[39]:
-import datetime as dt
-import random as rd
-
-
-def compare(n):
-    z=[]
-    for i in range(n):
-        r=rd.randint(0,100)
-        z.append(r)
-
-
-    t1=dt.datetime.now()
-    (counts(z))
-    t2=dt.datetime.now()
-
-
-
-    t3=dt.datetime.now()
-    (ins(z))
-    t4=dt.datetime.now()
-
-
-
-    print('counting sort:',t2-t1)
-    print('insertion sort:',t4-t3)
-    return
-
-compare(5000)
 
 #the time complexity for counting sort is o(n+k)
 #k is determined by the maximum value
 #its easy to see that counting sort easily beats o(n^2)
-#however its time complexity is much larger
+#however its space complexity is really large
+
+
+for i in range(100):
+    
+    target=rd.sample([i for i in range(1000)],100)
+
+    if counting_sort(target)!=sorted(target):
+        print('Erreur')
