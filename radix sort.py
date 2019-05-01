8c655054b3141cf70b4e34aa97f54de930c11af6
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-05-01 20:12:29 +0100

    restructure

--- radix sort.py
+++ radix sort.py
@@ -5,7 +5,6 @@ Created on Sun Apr 22 16:48:39 2018
 @author: Administrator
 """
 import random as rd
-import datetime as dt 
 
 #radix sort is fun
 #there are two ways to do it
@@ -13,17 +12,17 @@ import datetime as dt
 #the second one is to start from the left
 #in this context, i choose the first approach
 #they are equivalent
-#lets see we get the first digits of all elements
-#sort these elements based on the first digits
+#we get the first digit of all elements
+#sort these elements based upon the first digit
 #for instance, 215 and 19
 #9 for 19 is larger than 5 for 215
 #for the first round, we get 215,19
-#we move on to the second digits of all elements from the right
-#we apply the same technique of sorting based on digits
+#we move on to the second digit of all elements from the right
+#we apply the same technique of sorting based upon the second digit
 #until we reach to the very left digit of the maximum value in the list
 #the sort is completed
-#for our instance, the second round would be 215,19
-#since they both are 1
+#for instance, the second round would be 215,19
+#since the second digit of both numbers are 1
 #the final round is 19,215
 #as 0 for 19 is smaller than 2 for 215
 #period
@@ -36,20 +35,22 @@ import datetime as dt
 #i personally prefer to use a dictionary to store elements in its rightful place
 #i use digits from 0-9 as the key of dictionary
 #there are other elegant solutions, plz check the link at the very end
-def rad(n):
+def radix_sort(target):
     
     #the first part is to find the largest element in the list
     #next, we find out the length of it
     #so we can use the length in a loop to control how many digits we need to obtain
-    temp=len(str(max(n)))
+    temp=len(str(max(target)))
+    
     for i in range(temp):
         
         #each time, we clear the dictionary
         dictionary={'0':[],'1':[],'2':[],'3':[],'4':[],'5':[],'6':[],'7':[],'8':[],'9':[]}
         
-        #this is traversal on all elements in the list
-        for j in range(len(n)):
-            z=n[j]
+        #this is a traversal on all elements in the list
+        for j in range(len(target)):
+            
+            z=target[j]
             radix=10**i
             
             #(z//radix)%10) is the technique to get digits
@@ -58,77 +59,36 @@ def rad(n):
             #to store the element in its rightful place
             (dictionary[str((z//radix)%10)]).append(z)
             
-            #we have to empty the list after we put all elements into the dictionary
-        n=[]
+        #we have to empty the list after we put all elements into the dictionary
+        target=[]
         
         #this loop is unnecessary
-        #we can use n=dictionary[0]+...+dictionary[9]
+        #we can use target=dictionary[0]+...+dictionary[9]
         #again, i am a lazy person
-        #cut me some slack, plz
+        #cut me some slack, s’il vous plaît
         for k in range(10):
-            n+=dictionary[str(k)]
+            target+=dictionary[str(k)]
    
-    return n
-
-#
-
-def ins(list): 
-
-    for i in range(1,len(list)): 
-           val=list[i]
-           j=i 
-           while val<list[j-1] and j!=0: 
-                   list[j]=list[j-1]
-                   j-=1 
-           list[j]=val 
-    return list 
-
-
-#next, its our comparison with insertion sort
-def compare(n): 
-
-    z=[] 
-    for i in range(n):
-        r=rd.randint(0,1000000000000) 
-        z.append(r) 
-
-    
-#
-    t1=dt.datetime.now() 
-
-    a=(rad(z)) 
-
-    t2=dt.datetime.now() 
-
- 
-
-    t3=dt.datetime.now() 
-
-    b=(ins(z)) 
-
-    t4=dt.datetime.now() 
-
-# 
-    if a==b:
-        print(((t2-t1).microseconds)) 
-        print(((t4-t3).microseconds)) 
+    return target
 
 
 
-#surprisingly, the radix sort is faster
-
-#insertion is supposed to be o(n^2)
-#radix is o(k*n)
+#surprisingly, the radix sort is really fast
+#the time complexity of radix sort is o(k*n)
 #k depends on the digits of the maximum value in the list
 #well, its harder to get a number like 10^1000 than getting a list with 10^1000 elements
-#which makes radix sort is faster than insertion sort in this scenario
 #when we get a list with smaller n and larger k
-#insertion would be much faster!
+#radix sort may be a terrible choice
 
 #there are other ways of writing radix sort
 #i believe my way has too much space complexity
-#this one is actually smart and elegant
+#this one is rather smart and elegant
 # https://www.geeksforgeeks.org/radix-sort/
 
-compare(1000)
-        
+
+for i in range(100):
+    
+    target=rd.sample([i for i in range(1000)],100)
+
+    if radix_sort(target)!=sorted(target):
+        print('Erreur')
