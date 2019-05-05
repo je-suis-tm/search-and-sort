eea4ecb7d6cc930953c6e0259005b1190d09a3c6
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-05-05 15:49:59 +0100

    add comments

--- bitonic sort.py
+++ bitonic sort.py
@@ -9,8 +9,21 @@ import random as rd
 
 # In[2]:
 
+#bitonic sort is my special feature in this repository!!!
+#bitonic sort is cool as well as difficult to understand
+#if you look for detailed explanation, you should check out geeksforgeeks
+# https://www.geeksforgeeks.org/bitonic-sort/
+#its python version is written in recursion
+#which i found it really difficult to understand what the heck it was doing
+#so i spent the whole day scratching my head to finish this non-recursive version
+#a more straight forward explanation is the visualization from wikipedia
+# https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/BitonicSort1.svg/843px-BitonicSort1.svg.png
 
+#enjoy~~~
 def bitonic_sort(target):
+    
+    if len(target)==1:
+        return target
 
     i=1
     while 2**(i-1)<len(target)/2+1:
@@ -23,9 +36,11 @@ def bitonic_sort(target):
         
             for k in range(0,len(target),j):            
                 for l in range(k,k+int(j/2)):
-                    if target[l]>target[l+int(j/2)] and                     direction[l]==1:
+                    if target[l]>target[l+int(j/2)] and \
+                    direction[l]==1:
                         target[l],target[l+int(j/2)]=target[l+int(j/2)],target[l]
-                    if target[l]<target[l+int(j/2)] and                     direction[l]==-1:
+                    if target[l]<target[l+int(j/2)] and \
+                    direction[l]==-1:
                         target[l],target[l+int(j/2)]=target[l+int(j/2)],target[l]
             
             j=int(j/2)
@@ -62,14 +77,9 @@ def get_direction(iteration_number,length):
 
 for j in range(100):
     
-    target=[rd.randint(0,100) for i in range(2**rd.randint(1,10))]
+    #bitonic sort only works on a list with 2**n elements
+    target=[rd.randint(0,100) for i in range(2**rd.randint(0,10))]
     if bitonic_sort(target)!=sorted(target):
         print('Erreur')
 
-
-# In[4]:
-
-
-#https://www.geeksforgeeks.org/bitonic-sort/
-#https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/BitonicSort1.svg/843px-BitonicSort1.svg.pngR
 
