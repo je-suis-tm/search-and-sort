88d6c0c38ed48425a11c496fb2e0f5193dc53487
T.M <38026296+je-suis-tm@users.noreply.github.com>
2020-06-03 20:17:44 +0100

    remove reserved words

--- sequential and binary search.py
+++ sequential and binary search.py
@@ -38,8 +38,8 @@ def binary_search(target,raw_list):
     #binary search starts from the middle
     #if the middle value is larger, we search the lower half, vice versa
     #we do the same trick on the lower half recursively
-    #note that right=len(list)+1 can handle both odd and even number case
-    right=len(list)+1
+    #note that right=len(raw_list)+1 can handle both odd and even number case
+    right=len(raw_list)+1
     sorted_list=sorted(raw_list)
     found=False
     
@@ -48,7 +48,7 @@ def binary_search(target,raw_list):
         #to get the middle point of any length
         #we need to get the half of the sum of first and last index
         #we use (left+right)//2 to get the middle value for any length
-        #right=len(list)+1 can handle the odd number case
+        #right=len(raw_list)+1 can handle the odd number case
         i=(left+right)//2
         
         if sorted_list[i]==target:
